0bd379876b1152fadea8a692c079e131c6c6890d
Rohit <rohit.ravindra007@gmail.com>
2021-05-01 20:37:19 +0530

    Wrapping up

--- match-maker-frontend/src/Axios.js
+++ match-maker-frontend/src/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance=axios.create({
 
-    baseURL: 'http://localhost:8001'
+    baseURL: 'https://match-maker-backend.herokuapp.com'
 });
 
 export default instance;
